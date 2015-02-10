7c6a007e4a141d4d0c61c8ebed21fb757b07dfe2
Alexander Steshenko <CorvoOrc@users.noreply.github.com>
2015-02-10 09:34:39 +0400

    Update Family_member.cs

--- Family_member.cs
+++ Family_member.cs
@@ -17,9 +17,11 @@ namespace FilingRequestInBank
         private BindingSource bind = new BindingSource();
         private DataSet dataset = new DataSet();
         public int Id_family, Id_family_member;
+        
         public Family_member()
         {
             InitializeComponent();
+            
             FileInfo fi1 = new FileInfo("cur_req.txt");
             using (StreamReader sr = fi1.OpenText())
             {
@@ -40,7 +42,6 @@ namespace FilingRequestInBank
                     MessageBox.Show(@"Нет соединения с базой данных. Повторите запрос позднее!", @"Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
                 }
-
 
                 string str = String.Format(@"SELECT t1.Family_member_field as 'Family memder', t1.Activities as 'Activities', t1.Age as 'Age', t2.Surname as 'Surname' FROM Family t2 INNER JOIN Family_member t1 ON t1.Id_family=t2.Id_family WHERE t2.Id_family = '{0}'", Id_family);
                 SqlCommand cm = new SqlCommand(str, cn);
